d659e8d78ef4429d15b12519093db22fc9c65de3
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-05 11:07:28 -0300

    Done IHashService

--- src/services/HashService.ts
+++ src/services/HashService.ts
@@ -1,6 +1,11 @@
 import * as bcrypt from 'bcryptjs';
 
-export default class HashService {
+export interface IHashService {
+  hash(plainText: string): string;
+  compare(plainText: string, cypherText: string): boolean;
+}
+
+export default class HashService implements IHashService {
   public hash(plainText: string): string {
     const rounds = Number(process.env.BCRYPT_COST);
     const salt = bcrypt.genSaltSync(rounds);
