2dab252560b8871fe845c6678e203b5fb391fc03
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-05 11:20:45 -0300

    Add constructor/attributes in UserBusiness

--- src/business/user/UserBusiness.ts
+++ src/business/user/UserBusiness.ts
@@ -1,7 +1,20 @@
+import { IUserDatabase } from '../../data/UserDatabase';
 import User, { UserInputDTO } from '../../model/User';
+import { IHashService } from '../../services/HashService';
+import { IIdService } from '../../services/IdService';
+import { ITokenService } from '../../services/TokenService';
 import UserValidations from './UserValidations';
 
 export class UserBusiness extends UserValidations {
+  constructor(
+    private hashService: IHashService,
+    private idService: IIdService,
+    private tokenService: ITokenService,
+    private userDatabase: IUserDatabase
+  ) {
+    super();
+  }
+
   public async signup(data: UserInputDTO) {
     this.validateSignupFields(data);
 
