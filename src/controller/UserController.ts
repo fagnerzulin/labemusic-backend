4195651c04a23817792781e7733b08fb2a975fbe
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-05 11:38:32 -0300

    Done signup in UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
+import userBusiness from '../business/user/UserBusiness';
 import { UserInputDTO } from '../model/User';
 
 export default class UserController {
   public async signup(req: Request, res: Response): Promise<void> {
     try {
       const input: UserInputDTO = req.body;
+      const token = await userBusiness.signup(input);
+
+      res.status(201).send({ token });
     } catch (error) {
       res.status(error.statusCode || 400).send({ message: error.message });
     }
