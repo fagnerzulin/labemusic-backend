138bde745a037bf10c385883fcd83d3659b6fac3
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-05 11:28:24 -0300

    Done signup in UserBusiness

--- src/business/user/UserBusiness.ts
+++ src/business/user/UserBusiness.ts
@@ -1,8 +1,8 @@
-import { IUserDatabase } from '../../data/UserDatabase';
+import { IUserDatabase, UserDatabase } from '../../data/UserDatabase';
 import User, { UserInputDTO } from '../../model/User';
-import { IHashService } from '../../services/HashService';
-import { IIdService } from '../../services/IdService';
-import { ITokenService } from '../../services/TokenService';
+import HashService, { IHashService } from '../../services/HashService';
+import IdService, { IIdService } from '../../services/IdService';
+import TokenService, { ITokenService } from '../../services/TokenService';
 import UserValidations from './UserValidations';
 
 export class UserBusiness extends UserValidations {
@@ -15,14 +15,28 @@ export class UserBusiness extends UserValidations {
     super();
   }
 
-  public async signup(data: UserInputDTO) {
+  public async signup(data: UserInputDTO): Promise<string> {
     this.validateSignupFields(data);
 
-    // const user: User = new User(
-    //   data.name,
-    //   data.email,
-    //   data.nickname,
-    //   data.password
-    // );
+    const id: string = this.idService.idGenerate();
+
+    const user: User = new User(
+      data.name,
+      data.email,
+      data.nickname,
+      this.hashService.hash(data.password),
+      id
+    );
+
+    await this.userDatabase.insertUser(user.userToDb());
+
+    return this.tokenService.generateToken({ id });
   }
 }
+
+export default new UserBusiness(
+  new HashService(),
+  new IdService(),
+  new TokenService(),
+  new UserDatabase()
+);
