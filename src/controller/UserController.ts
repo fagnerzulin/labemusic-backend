fa2ff8e0f0ada0509fc6b0beab7f57955f9be680
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-05 10:04:13 -0300

    Start to do signup in UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
+import { UserInputDTO } from '../model/User';
 
 export default class UserController {
-  public async signup(req: Request, res: Response): Promise<void> {}
+  public async signup(req: Request, res: Response): Promise<void> {
+    try {
+      const input: UserInputDTO = req.body;
+    } catch (error) {
+      res.status(error.statusCode || 400).send({ message: error.message });
+    }
+  }
 }
