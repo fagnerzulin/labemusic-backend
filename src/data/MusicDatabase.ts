5319dc45c680dbdd110a44b6b4d747e867595ce1
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-08 14:56:42 -0300

    Add selectById method in MusicDatabase

--- src/data/MusicDatabase.ts
+++ src/data/MusicDatabase.ts
@@ -5,6 +5,7 @@ import CustomError from '../error/CustomError';
 export interface IMusicDatabase {
   insertMusic(music: MusicToDB): Promise<void>;
   selectAll(author: string): Promise<Music[]>;
+  selectById(id: string): Promise<Music>;
 }
 
 export class MusicDatabase extends BaseDatebase implements IMusicDatabase {
@@ -45,7 +46,7 @@ export class MusicDatabase extends BaseDatebase implements IMusicDatabase {
         .join(`${this.genreTable} as gt`, 'gt.id', 'mgt.genre_id')
         .where('mt.author', author);
 
-      const data = this.tranformData(result);
+      const data = this.transformData(result);
 
       return data.map(
         music =>
@@ -64,7 +65,48 @@ export class MusicDatabase extends BaseDatebase implements IMusicDatabase {
     }
   }
 
-  private tranformData(data: any): any[] {
+  public async selectById(id: string): Promise<Music> {
+    try {
+      const result = await BaseDatebase.knexConnection(
+        `${this.musicTable} as mt`
+      )
+        .select(
+          'mt.id as id',
+          'subtitle',
+          'ut.name as author',
+          'date',
+          'file',
+          'at.album as album',
+          'gt.genre as genre'
+        )
+        .join(`${this.userTable} as ut`, 'ut.id', 'mt.author')
+        .join(`${this.albumTable} as at`, 'at.id', 'mt.album')
+        .join(`${this.musicHasGenreTable} as mgt`, 'mgt.music_id', 'mt.id')
+        .join(`${this.genreTable} as gt`, 'gt.id', 'mgt.genre_id')
+        .where('mt.id', id);
+
+      const [data] = this.transformData(result);
+
+      if (!data) throw new CustomError('Music not found', 404);
+
+      return new Music(
+        data.id,
+        data.subtitle,
+        data.author,
+        data.file,
+        data.genre,
+        data.album,
+        data.date
+      );
+    } catch (error) {
+      throw new CustomError(
+        error.message || error.sqlMessage,
+        error.statusCode || 500
+      );
+    }
+  }
+
+  private transformData(data: any): any[] {
     const transformedData: any[] = [];
     for (const firstValue of data) {
       if (!transformedData.find(value => value.id === firstValue.id)) {
